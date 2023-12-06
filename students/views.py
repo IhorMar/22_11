0e4227049b119c0c120af345fa5a89e05061d5d8
IhorMar <rmg1203@gmail.com>
2023-12-06 16:42:21 +0200

    made changes and add file

--- students/views.py
+++ students/views.py
@@ -1,36 +1,66 @@
-import json
+from django.http import HttpResponse
+from django.shortcuts import render
 
-from django.http import HttpResponse, Http404
+# from django.shortcuts import render
 
 
 # Create your views here.
-def students_main(request):
-    return HttpResponse("<h1>Some text in the browser</h1>")
+# def students_main(request):
+#     return HttpResponse(
+#         "<h1>Some text in the browser</h1><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>")
 
 
-def students_personal(request, sid):
+# def students_personal(request, sid):
+#     breakpoint()
+#     try:
+#         sid = int(sid)
+#     except ValueError:
+#         raise Http404
+#     else:
+#         return HttpResponse("<h1>Some text</h1>" f"<h2><p>{sid}</p><h2>")
+
+
+# def parse_request(request):
+#     request_info = {
+#         "method": request.method,
+#         "path": request.path,
+#         "GET_params": dict(request.GET),
+#         "POST_params": dict(request.POST),
+#         "headers": dict(request.headers),
+#         "user_agent": request.headers.get("User-Agent"),
+#         "remote_address": request.META.get("REMOTE_ADDR"),
+#     }
+#
+#     response_data = json.dumps(request_info, indent=4)
+#
+#     response_html = f"<pre>{response_data}</pre>"
+#
+#     return HttpResponse(response_html)
+
+
+# def main_page(request):
+#     template = loader.get_template('students/index.html')
+#     context = {}
+#     return HttpResponse(template.render(context))
+
+
+def demonstration_page(request):
+    return render(request, "students/demonstration.html", {})
+
+
+# Students views
+def students_list(request):
+    return render(request, "students/index.html", {})
+
+
+def students_add(request):
     # breakpoint()
-    try:
-        sid = int(sid)
-    except ValueError:
-        raise Http404
-    else:
-        return HttpResponse("<h1>Some text</h1>" f"<h2><p>{sid}</p><h2>")
+    return HttpResponse("<h1>Student Add Form</h1>")
 
 
-def parse_request(request):
-    request_info = {
-        "method": request.method,
-        "path": request.path,
-        "GET_params": dict(request.GET),
-        "POST_params": dict(request.POST),
-        "headers": dict(request.headers),
-        "user_agent": request.headers.get("User-Agent"),
-        "remote_address": request.META.get("REMOTE_ADDR"),
-    }
+def students_edit(request, sid):
+    return HttpResponse("<h1>Edit Student %s</h1>" % sid)
 
-    response_data = json.dumps(request_info, indent=4)
 
-    response_html = f"<pre>{response_data}</pre>"
-
-    return HttpResponse(response_html)
+def students_delete(request, sid):
+    return HttpResponse("<h1>Delete Student %s</h1>" % sid)
--- students_2911/urls.py
+++ students_2911/urls.py
@@ -1,16 +1,30 @@
 from django.contrib import admin
 from django.urls import path
 
-from students import views as students_main
+from students import views as all_views
 
 urlpatterns = [
+    # path(
+    #     "test/", students_main.students_main, name="test_page"
+    # ),  # Дві урли повісили на одну вюшку(students_main)
+    # path(
+    #     "empty/", students_main.students_main, name="test_page"
+    # ),  # Дві урли повісили на одну вюшку(students_main)
+    #
+    # path("parse_request/", students_main.parse_request, name="parse_request"),
+    # path("main_2/", students_main.main_page_render, name="main_2"),
+    # path("test/<str:sid>/", students_main.students_personal, name="test_page_person"),
+    path(
+        "demonstration_page/", all_views.demonstration_page, name="demonstration_page"
+    ),
     path("admin/", admin.site.urls),
+    # Students url
+    path("", all_views.students_list, name="main"),
+    path("students/add/", all_views.students_add, name="students_add"),
+    path("students/<int:sid>/edit/", all_views.students_edit, name="students_edit"),
     path(
-        "test/", students_main.students_main, name="test_page"
-    ),  # Дві урли повісили на одну вюшку(students_main)
-    path(
-        "", students_main.students_main, name="test_page"
-    ),  # Дві урли повісили на одну вюшку(students_main)
-    path("parse_request/", students_main.parse_request, name="parse_request"),
-    path("test/<str:sid>/", students_main.students_personal, name="test_page_person"),
+        "students/<int:sid>/delete/", all_views.students_delete, name="students_delete"
+    ),
+    # Groups url
+    # Journals url
 ]
